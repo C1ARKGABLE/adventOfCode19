b161161cd861d9eb6be30276f83dee083829f057
Brown <Gable.Brown@disney.com>
2019-12-13 14:19:54 -0500

    more work

--- day_12/cheat.py
+++ day_12/cheat.py
@@ -1,71 +1,78 @@
-"""
-1. Simulate every moon in a time step (e.g on iteration of a loop)
-2. Update VELOCITY by applying GRAVITY
-3. Update POSITION by applying VELOCITY
-"""
-
+#<x=3, y=2, z=-6>
+#<x=-13, y=18, z=10>
+#<x=-8, y=-1, z=13>
+#<x=5, y=10, z=4>
 from copy import deepcopy
+import math
 
-
-def gcd(a, b):
-    while b > 0:
-        a, b = b, a % b
-    return a
-
-
-def lcm(a, b):
-    return (a*b)//gcd(a, b)
-
-
-def tick():
-    """
-    A single tick amounts to a new calculation of positions and velocities for all the moons.
-    """
-    for i in range(len(Mpos)):
-        for j in range(len(Mpos)):
-            for p in Mpos[i]:
-                if Mpos[i][p] < Mpos[j][p]:
-                    Mvel[i][p] += 1
-                elif Mpos[i][p] > Mpos[j][p]:
-                    Mvel[i][p] -= 1
-
-    # Update the x,y,z position of each moon based on its x,y,z velocity.
-    for i in range(len(Mpos)):
-        for k in Mpos[i]:
-            Mpos[i][k] += Mvel[i][k]
-
-
-Mpos = []
-Mvel = []
-
-for line in open("input.txt", "r").readlines():
+MP = []
+MV = []
+for line in open('input.txt').readlines():
     line = line.strip()
     line = line[1:-1]
-    line = line.split(", ")
+    words = line.split()
     moon = {}
-    for e in line:
-        k, v = e.split("=")
+    for w in words:
+        k,v = w.split('=')
+        if v.endswith(','):
+            v = v[:-1]
         moon[k] = int(v)
-    Mpos.append(moon)
-    Mvel.append({'x': 0, 'y': 0, 'z': 0})
+    MP.append(moon)
+    MV.append({'x': 0, 'y': 0, 'z': 0})
 
-# Make copies of the stating positions and velocities (for Part 2)
-start_Mpos = deepcopy(Mpos)
-start_Mvel = deepcopy(Mvel)
+def tick():
+    for i in range(len(MP)):
+        for j in range(len(MP)):
+            for k in MP[i]:
+                if MP[i][k] < MP[j][k]:
+                    MV[i][k] += 1
+                if MP[i][k] > MP[j][k]:
+                    MV[i][k] -= 1
+    for i in range(len(MP)):
+        for k in MP[i]:
+            MP[i][k] += MV[i][k]
 
-
-# For Part 1 we just need to run 1000 ticks and then calculate some stuff. Fairly straightforward once you have a
-# feel for what the question is asking.
-part_1 = 0
+OLD_MP = deepcopy(MP)
+OLD_MV = deepcopy(MV)
 for _ in range(1000):
     tick()
+p1 = 0
+for i in range(len(MP)):
+    pot = 0
+    kin = 0
+    for k in MP[i]:
+        pot += abs(MP[i][k])
+        kin += abs(MV[i][k])
+    p1 += pot*kin
+print(p1)
 
-for i in range(len(Mpos)):
-    pot = 0  # Potential energy. Sum of absolute values of moon's co-ordinates.
-    kin = 0  # Kinetic energy. Sum of absolute values of its velocity coordinates
-    for k in Mpos[i]:
-        pot += abs(Mpos[i][k])
-        kin += abs(Mvel[i][k])
-    part_1 += (pot * kin)
-
-print(part_1)
+MP = OLD_MP
+MV = OLD_MV
+SEEN = {k: {} for k in MP[0]}
+CNT = {k: 0 for k in MP[0]}
+t = 0
+p2 = 1
+p2_cnt = 0
+while p2_cnt < 3:
+    tick()
+    KEY = {k: [] for k in MP[0]}
+    for i in range(len(MP)):
+        for k in MP[i]:
+            KEY[k].append(MP[i][k])
+            KEY[k].append(MV[i][k])
+    KEY = {k: tuple(v) for k,v in KEY.items()}
+    for k in KEY:
+        if KEY[k] in SEEN[k]:
+            if CNT[k] == 0:
+                assert SEEN[k][KEY[k]] == 0
+                # Say X repeats after tX, Y repeats after tY
+                # Then (X,Y) will repeat after lcm(tX,tY) = tX*tY/gcd(tX,tY)
+                # lcm(A,B,C) = lcm(lcm(A,B),C)
+                p2 = p2*t//math.gcd(p2,t)
+                p2_cnt += 1
+                if p2_cnt == 3:
+                    print(p2)
+                    break
+            CNT[k] += 1
+        SEEN[k][KEY[k]] = t
+    t += 1
--- day_12/main.py
+++ day_12/main.py
@@ -1,4 +1,12 @@
 import numpy as np
+from datetime import datetime
+import copy
+from math import gcd
+
+
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
+
 
 pos = []
 for line in open("input.txt", "r").readlines():
@@ -11,37 +19,69 @@ for line in open("input.txt", "r").readlines():
 class Moon:
     def __init__(self, name, pos):
         self.name = name
-        self.pos = np.array(pos)
-        self.vel = np.zeros((3))
+        self.pos = np.array(pos, dtype=int)
+        self.vel = np.zeros((3), dtype=int)
+
+    def get_x(self):
+        return self.pos[0]
+
+    def get_y(self):
+        return self.pos[1]
+
+    def get_z(self):
+        return self.pos[2]
 
     def move(self):
         self.pos += self.vel
 
     def gravity(self, other):
+        # print(other.pos)
 
-        diff =  other.pos - self.pos
+        # self 3 other 5
+        # self +1 other -1 because 3 < 5
+
+        diff = other.pos - self.pos  # 2
         normal = 1 * (diff > 0) - (diff < 0)
 
+        # print(normal)
+
         self.vel += normal
-        #other.vel -= normal
+        other.vel -= normal
 
     def energy(self):
         return sum(abs(self.pos)) * sum(abs(self.vel))
 
-class System:
 
-    def __init__(self,moons):
+class System:
+    def __init__(self, moons):
         self.moons = moons
+
+    def summary(self):
+        return [f"{m.name} {m.pos = }, {m.vel = }" for m in self.moons]
+
+    def get_coords(self, axis):
+        if axis == "x":
+            return sum(m.get_x() * 100 ^ i for i, m in enumerate(self.moons))
+        elif axis == "y":
+            return sum(m.get_y() * 100 ^ i for i, m in enumerate(self.moons))
+        elif axis == "z":
+            return sum(m.get_z() * 100 ^ i for i, m in enumerate(self.moons))
+        else:
+            return None
+
     def energy(self):
         return sum(m.energy() for m in self.moons)
+
     def step(self):
         temp = self.moons.copy()
 
-        for moon in temp:
-            for moon2 in self.moons:
-                if not moon == moon2:
-                    moon.gravity(moon2)
+        for moon in self.moons:
+            temp.remove(moon)
+            for moon2 in temp:
+                moon.gravity(moon2)
 
+        [m.move() for m in self.moons]
+        [print(self.get_coords(i)) for i in ["x","y","z"]]
 
 
 """
@@ -54,54 +94,53 @@ class System:
 # print(vels)
 planets = ["Io", "Europa", "Ganymede", "Callisto"]
 
-motion = {}
-
 moons = [Moon(planets[i], p) for i, p in enumerate(pos)]
 
 system = System(moons)
 
-for i in range(1000):
-    system.step()
-
-print(system.energy())
-
-# def gravity(motion, pts):
-
-#     for source in pts:
-#         pts.remove(source)
-#         for dest in pts:
-
-#             diff = motion[dest]["pos"] - motion[source]["pos"]
-
-#             # Sprint(1*(diff > 0) - (diff < 0))
-
-#             motion[dest]["vel"] -= 1 * (diff > 0) - (diff < 0)
-#             motion[source]["vel"] += 1 * (diff > 0) - (diff < 0)
-
-#     return motion
-
-
-# def move(motion, planets):
-#     for planet in planets:
-#         motion[planet]["pos"] += motion[planet]["vel"]
-
-#     return motion
-
-
-# def energy(motion, planets):
-#     total = 0
-#     for planet in planets:
-#         total += sum(abs(motion[planet]["pos"])) * sum(abs(motion[planet]["vel"]))
-#     return total
-
-
 # for i in range(1000):
-#     motion = gravity(motion, planets.copy())
-#     motion = move(motion, planets)
+#     print("="*30)
+#     print(f"step {i}")
+#     system.step()
+#     system.summary()
 
-# print(energy(motion, planets))
-# # print(motion)
+# print(system.energy()) # 6227
 
 
-# print(np.array([3, 4, 5]) - np.array([1, 2, 3]))
+# start_state = copy.deepcopy(system)
 
+xs = [system.get_coords("x")]
+ys = [system.get_coords("y")]
+zs = [system.get_coords("z")]
+
+x_done, y_done, z_done = False, False, False
+count = 0
+
+print(xs, ys, zs)
+while not (x_done and y_done and z_done):
+    # if count % 10000 == 0:
+    #     print(count)
+    #     print(x_done,y_done,z_done)
+    system.step()
+    if (x := system.get_coords("x")) in xs: 
+        x_done = True
+        x_count = count
+    if (y := system.get_coords("y")) in ys:
+        y_done = True
+        y_count = count
+    if (z := system.get_coords("z")) in zs:
+        z_done = True
+        z_count = count
+
+    xs.append(x)
+    ys.append(y)
+    zs.append(z)
+
+    count += 1
+
+print(xs, ys, zs)
+print(x_count, y_count, z_count)
+print(lcm(x_count, lcm(y_count, z_count)))
+
+
+# print(abs(np.array([1,-2,10,-100])) + np.array([2,3,10,2]))
